0c2c8692c8daaf9c366a5a8dc72d7f855f78d5c7
Thomas <2201871@uad.ac.uk>
2024-10-17 17:57:55 +0100

    Fixed entry point

--- Bolt Engine/Core.h
+++ Bolt Engine/Core.h
@@ -2,11 +2,10 @@
 
 #ifdef BOLT_PLATFORM_WINDOWS // States we are working on Windows
 	#ifdef BOLT_BUILD_DLL
-	#define BOLT_ENGINE_API __declspec(dllexport) // Sets up the dll for the Bolt Engine and not the Game layer
-#else
-	#define BOLT_ENGINE_API __declspec(dllimport)
-#endif
+		#define BOLT_ENGINE_API __declspec(dllexport) // Sets up the dll for the Bolt Engine and not the Game layer
+	#else
+		#define BOLT_ENGINE_API __declspec(dllimport)
+	#endif
 #else
 #error Bolt Engine only supports Windows
-	
 #endif
